5602799f6d080fb7571a26d8e75c3e8b9c31e7a4
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2024-04-24 07:03:06 +0700

    init project

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	let name = 'Max';
+	let age = 30;
+
+	$: uppercaseName = name.toUpperCase();
+
+	function incrementAge() {
+		age += 1;
+	}
+
+	function changeName() {
+		name = 'Thuan Nguyen';
+	}
+	function nameInput(event) {
+		const enteredValue = event.target.value;
+		name = enteredValue;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {uppercaseName}, my age is {age + 1}!</h1>
+<button on:click="{incrementAge}">Change Age</button>
+<!-- <button on:click={changeName}>Change Name</button> -->
+<input type="text" value={name} on:input={nameInput}/>
