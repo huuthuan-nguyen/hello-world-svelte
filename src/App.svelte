b16b10575cdd8f516e62de8df037964ce171e837
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2024-05-09 02:10:31 +0700

    update contacts list

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,30 @@
 	let title = "";
 	let image = "";
 	let description = "";
+	let formState = "empty";
+	let createdContacts = [];
+
+	function addContact() {
+		if (
+			name.trim().length == 0 ||
+			title.trim().length == 0 ||
+			image.trim().length == 0 ||
+			description.trim().length == 0
+		) {
+			formState = "invalid";
+			return;
+		}
+		createdContacts = [
+			...createdContacts,
+			{
+				name: name,
+				jobTitle: title,
+				imageUrl: image,
+				desc: description,
+			},
+		];
+		formState = "done";
+	}
 </script>
 
 <div id="form">
@@ -26,7 +50,25 @@
 	</div>
 </div>
 
-<ContactCard userName={name} jobTitle={title} {description} userImage={image} />
+<button on:click={addContact}>Add Contact Card</button>
+
+{#if formState === "invalid"}
+	<p>Invalid input.</p>
+{:else}
+	<p>Please enter some data and hit the button!</p>
+{/if}
+
+{#each createdContacts as contact, index}
+	<h2># {index + 1}</h2>
+	<ContactCard
+		userName={contact.name}
+		jobTitle={contact.jobTitle}
+		description={contact.desc}
+		userImage={contact.imageUrl}
+	/>
+{:else}
+	<p>Please start adding some contacts, we found none!</p>
+{/each}
 
 <style>
 	#form {
